110f2800d8356c00cab62f35ce0f9d11560982b9
LombardiMEllis <30663484+LombardiMEllis@users.noreply.github.com>
2017-08-13 14:25:43 -0400

    comments added

--- src/U1L01Warmup.java
+++ src/U1L01Warmup.java
@@ -3,15 +3,18 @@ import javax.swing.JOptionPane;
 public class U1L01Warmup {
 
 	public static void main(String[] args) {
+		//set-up variables and ask for input
 		String input = JOptionPane.showInputDialog(null, "Enter a positive, nonnegative integer: ");
 		int number = Integer.parseInt(input);
 		int answer = 1;
 		
+		//calculate
 		while (number > 0){
 			answer = answer * number;
 			number--;
 		}
 		
+		//print out result
 		System.out.println("The result is: " + answer);
 
 	}
